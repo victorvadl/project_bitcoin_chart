0db2e1b10cd8754b78ce263485367a1bf4535c90
Victor Rafael <99365539+victorvadl@users.noreply.github.com>
2022-07-07 13:02:25 -0300

    Add files via upload
    Imported my project that have a bitcoin candlestick chart over past 90 days

---
+++ bitcoin_chart.py
@@ -0,0 +1,24 @@
+from pycoingecko import CoinGeckoAPI
+import pandas as pd
+import plotly.offline as plotly
+import plotly.graph_objs as go
+
+plot = plotly.plot
+
+cg = CoinGeckoAPI()
+
+bitcoin_data = cg.get_coin_market_chart_by_id(id="bitcoin", vs_currency="usd", days=90)
+bitcoin_price_data = bitcoin_data["prices"]
+data= pd.DataFrame(bitcoin_price_data, columns=["TimeStamp", "Price"])
+data["Date"] = pd.to_datetime(data["TimeStamp"], unit="ms")
+candlestick_data = data.groupby(data.Date.dt.date).agg({"Price": ["min", "max", "first" , "last"]})
+
+fig = go.Figure(data=[go.Candlestick(x= candlestick_data.index, 
+open = candlestick_data["Price"]["first"], 
+high = candlestick_data["Price"]["max"], 
+low = candlestick_data["Price"]["min"], 
+close=candlestick_data["Price"]["last"])])
+
+fig.update_layout(xaxis_rangeslider_visible=False, xaxis_title="Date", yaxis_title="Price (USD $)", title = "Bitcoin Candlestick Chart Over Past 90 Days")
+
+plot(fig, filename ="bitcoin_candlestick_graph.html")
